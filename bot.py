2c18f86e8220438b30ccf6dd7550310ef0ea7d13
Shen0000 <bshen.324@gmail.com>
2020-11-06 16:12:07 -0500

    first version of my discord bot!

--- bot.py
+++ bot.py
@@ -1,1 +1,42 @@
 import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="!")
+cli = discord.Client()
+
+@client.command()
+async def server(ctx):
+    name = str(ctx.guild.name)
+    desc = str(ctx.guild.description)
+    owner = str(ctx.guild.owner)
+    id = str(ctx.guild.id)
+    region = str(ctx.guild.region)
+    memberCount = str(ctx.guild.member_count)
+
+    icon = str(ctx.guild.icon_url)
+
+    embed = discord.Embed(
+        title=name + " Server Information",
+        Description=desc,
+        color=discord.Color.blue()    
+    )
+    embed.set_thumbnail(url=icon)
+    embed.add_field(name="Owner", value=owner, inline=True)
+    embed.add_field(name="Server ID", value=id, inline=True)
+    embed.add_field(name="Region", value=region, inline=True)
+    embed.add_field(name="Member Count", value=memberCount, inline=True)
+
+    await ctx.send(embed=embed)
+
+@client.command()
+async def hello(ctx, *args):
+    for arg in args:
+        await ctx.send(arg)
+
+@cli.event
+async def on_message(message):
+    if str(message.channel.type) == "private":
+        modmail_channel = discord.utils.get(client.get_all_channels(), name="mod-mail")
+        await modmail_channel.send("["+ message.author.display_name + "] " + message.content)
+
+client.run("Nzc0MDgzNTExNDA1MjQ4NTMy.X6Sncg.2miOxdZQkRdwku6K9bRGHb0xWOM")
--- first_try.py
+++ first_try.py
@@ -1,2 +1,32 @@
 import discord
+from discord.ext import commands
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    message.content.lower()
+    if message.author == client.user:
+        return
+    if message.content.startswith("!Shenbot"):
+        if str(message.author) == "B$hens#7985":
+            await message.channel.send("Hello " + str(message.author) + " the master!")
+        else:
+            await message.channel.send("I am Shenbot")
+    if message.content.startswith("!urmom"):
+        await message.channel.send("Gay")
+    if message.content.startswith("!joke"):
+        await message.channel.send("Your life")
+    if str(message.author) == "ManimanTestBot#2912":
+        await message.channel.send("ManimanTestBot is my good friend")
+    if str(message.channel) == "delete-text" and message.content != "":
+        await message.channel.purge(limit=2)
+    if str(message.channel.type) == "private":
+        modmail_channel = discord.utils.get(client.get_all_channels(), name="mod-mail")
+        await modmail_channel.send("["+ message.author.display_name + "] " + message.content)
+    if str(message.channel.type) == "private":
+        modmail_channel = discord.utils.get(client.get_all_channels(), name="bot-mail")
+        await modmail_channel.send("["+ message.author.display_name + "] " + message.content)
+
+client.run("Nzc0MDgzNTExNDA1MjQ4NTMy.X6Sncg.2miOxdZQkRdwku6K9bRGHb0xWOM")
 
