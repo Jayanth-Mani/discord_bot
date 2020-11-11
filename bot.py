f06484176abba658b130bed9f1cc33a0ad3e998d
Jayanth-Mani <69757920+Jayanth-Mani@users.noreply.github.com>
2020-11-11 09:05:06 -0500

    cleaned up the code

--- bot.py
+++ bot.py
@@ -12,20 +12,19 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online,activity=discord.Game(";;being a bot."))
     print("ManimanBot is ready.")
 
-@client.command()
+@client.command(name = "echo", help = "Returns message.")
 async def echo(ctx, *args):
     message = ''
     for arg in args:
        message = message + " " + arg
     await ctx.send(message)
 
-@client.command()
+@client.command(name = "joke", help = "scrapes a joke from https://bestlifeonline.com/hilariously-silly-jokes/'")
 async def joke(ctx):
     await ctx.send(getJoke())    
-load_dotenv(".env") # replace ".env" with whatever you named your file
-client_token = os.environ.get('TOKEN')
 
-@client.command()
+
+@client.command(name = 'youtube', help = "gives the top video of a search")
 async def youtube(ctx, *args):
     titleSearch = ""
     for arg in args:
@@ -33,5 +32,12 @@ async def youtube(ctx, *args):
 
     url = "https://www.youtube.com/watch?v=" + str(getVideo(titleSearch))
     await ctx.send("Here is the top result: " + url)
+
+@client.command(name = 'ping', help = "gives ping of bot.")
+async def ping(ctx):
+    await ctx.send("Pong! {}ms".format(round(client.latency*1000)))
+
+load_dotenv(".env") # replace ".env" with whatever you named your file
+client_token = os.environ.get('TOKEN')
 
 client.run(client_token)
